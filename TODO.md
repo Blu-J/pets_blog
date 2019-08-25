4add8c5889cf0067877925fef2ced2d1bd30d872
Justin Miller <mogulslayer@gmail.com>
2019-08-25 13:03:19 -0600

    Add the deploy

--- TODO.md
+++
@@ -1,4 +0,0 @@
-TODO:
-
-1. Create single page per image
-1. Create the all
--- src/routes/petImage/_pets.js
+++ src/routes/petImage/_pets.js
@@ -1,6 +1,30 @@
-const all = "IMG_20190531_211627.jpg IMG_20190731_220809.jpg IMG_20190731_220847.jpg IMG_20190731_221144.jpg IMG_20190617_201834.jpg IMG_20190731_220816.jpg IMG_20190731_220927.jpg IMG_20190731_221226.jpg IMG_20190730_204943.jpg IMG_20190731_220821.jpg IMG_20190731_221115.jpg IMG_20190731_221721.jpg IMG_20190731_220803.jpg IMG_20190731_220843.jpg IMG_20190731_221120.jpg IMG_20190731_221728.jpg"
-  .split(" ")
-  .map(RegExp.prototype.exec.bind(/(.*?)\.jpg/))
-  .filter(x => x.length)
-  .map(([, value]) => value);
+import { compose, match, map, reject, isNil, prop } from "ramda";
+const files = [
+  "service-worker-index.html",
+  "favicon.png",
+  "global.css",
+  "manifest.json",
+  "pets/IMG_20190531_211627.jpg",
+  "pets/IMG_20190617_201834.jpg",
+  "pets/IMG_20190730_204943.jpg",
+  "pets/IMG_20190731_220803.jpg",
+  "pets/IMG_20190731_220809.jpg",
+  "pets/IMG_20190731_220816.jpg",
+  "pets/IMG_20190731_220821.jpg",
+  "pets/IMG_20190731_220843.jpg",
+  "pets/IMG_20190731_220847.jpg",
+  "pets/IMG_20190731_220927.jpg",
+  "pets/IMG_20190731_221115.jpg",
+  "pets/IMG_20190731_221120.jpg",
+  "pets/IMG_20190731_221144.jpg",
+  "pets/IMG_20190731_221226.jpg",
+  "pets/IMG_20190731_221721.jpg",
+  "pets/IMG_20190731_221728.jpg"
+];
+const getJpgs = compose(
+  reject(isNil),
+  map(prop(1)),
+  map(match(/pets\/(.*?).jpg/))
+);
+const all = getJpgs(files);
 export default all;

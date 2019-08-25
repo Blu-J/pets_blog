2b0d60ebe6a9aaf922e4cd65a7706ab26f052eeb
Justin Miller <mogulslayer@gmail.com>
2019-08-25 13:36:15 -0600

    chore: try 2?

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
-    "/pets_blog",
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()

5ebeb4544076cbb4483eb88d4b5d0066a335e627
Justin Miller <mogulslayer@gmail.com>
2019-08-26 06:40:45 -0600

    chore: Migrate domain owned functions

--- src/routes/petImage/_pets.js
+++ src/routes/petImage/_pets.js
@@ -1,6 +1,25 @@
 // @ts-check
 /** @typedef {string & {__type: 'petName'}} PetName */
-import { compose, match, map, reject, isNil, path } from "ramda";
+/** @typedef { number & {__t: 'petIndex'} } PetIndex */
+/** @typedef {import('ramda')}  R*/
+import {
+  __,
+  always,
+  compose,
+  equals,
+  findIndex,
+  identity,
+  ifElse,
+  isNil,
+  lensIndex,
+  map,
+  match,
+  mathMod,
+  path,
+  reject,
+  view
+} from "ramda";
+
 const files = [
   "service-worker-index.html",
   "favicon.png",
@@ -29,11 +48,49 @@ const getImages = compose(
   map(path([1])),
   map(match(/pets\/(.*?).jpg/))
 );
-const all = getImages(files);
-export default all;
+const pets = getImages(files);
+export default pets;
 
-console.assert(all.length >= 2, "Expecting that we have more than 2 images");
+const boundToIndex = /** @type {function(number): PetIndex} */ (mathMod(
+  __,
+  pets.length
+));
+/** @type {function(PetName): PetIndex} */
+export const getFoundIndex = compose(
+  ifElse(equals(-1), always(0), identity),
+  // @ts-ignore
+  findIndex(__, pets),
+  equals
+);
+/** @type {function(number): PetName} */
+export const getAtIndex = compose(
+  // @ts-ignore
+  view(__, pets),
+  lensIndex,
+  boundToIndex
+);
+
+console.assert(pets.length >= 2, "Expecting that we have more than 2 images");
 console.assert(
-  all.length < files.length,
+  pets.length < files.length,
   "The images length should be less than input"
+);
+
+console.assert(boundToIndex(pets.length) === 0, "0 -> 0 for index mapping");
+console.assert(
+  boundToIndex(-1) === pets.length - 1,
+  "should use modulo via math"
+);
+
+console.assert(getFoundIndex(pets[1]) === 1, "Should find first element");
+
+console.assert(
+  // @ts-ignore
+  getFoundIndex("pets[1]") === 0,
+  `getFoundIndex("pets[1]") should default to 0`
+);
+
+console.assert(
+  getFoundIndex(getAtIndex(boundToIndex(1))) === 1,
+  "getFoundIndex(getAtIndex(boundToIndex(1))) should be identity"
 );
